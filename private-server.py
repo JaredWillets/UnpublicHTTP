2c38835ba13552353e6238ca66a47e7b377e71e4
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2021-08-25 06:12:06 +0900

    started to take things from queue into a separate list

--- private-server.py
+++ private-server.py
@@ -1,4 +1,19 @@
 import requests
 import time
+import routing
+import json
+
 publicServerAddress = '[insert your public server address here]'
-command = requests.post(publicServerAddress)
+
+while True:
+
+    command = json.load(requests.post(publicServerAddress).text)
+    output = routing.run(command)
+    response = requests.post(publicServerAddress, data = output).text
+    if response != 'ok':
+
+        print('process not handled correctly')
+
+    else:
+        
+        continue
--- public-server.py
+++ public-server.py
@@ -5,6 +5,7 @@ from flask import Flask, requets
 queueRoute = '[ Insert the path that you would like for the queue ]' ## Replace this or the program will fail
 queue = [] ## This will be used to store the requests that have not been tended to
 queuePassword = 'password' ## Though this code will work, we recommend changing the password here
+returnRoute = '[ Insert the patht ath you would like for information to be returned to ]' ## Replace this or the program will not work
 
 app = Flask(__name__)
 
@@ -17,14 +18,19 @@ def getPath(path):
             return requested
         else:
             return 'incorrect queue password'
-    elif path == f'{queueRoute}' and not request.method=='GET':
+    elif (path == f'{queueRoute}' or path == f'{returnRoute}')and not request.method=='POST':
         return 'inocrrect method'
+    elif path == f'{returnRoute}' and method == 'POST':
+        output = request.form['output']
+        key = request.form['key']
+        return 'ok'
     else:
         start = time.time()
-        key = len(queue)
+        key = len(processing)
         queue.append({'path':path,'key':key,'output':''})
+        processing.append({'path':'key':key,'output':''})
         while time.time()-start <= 5:
-            output = queue[key]['output']
+            output = processing[key]['output']
             if output != '':
                 return output
             else:
