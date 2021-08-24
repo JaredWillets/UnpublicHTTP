a895150cb2f47db423375537cb52a9b61bdf83fb
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2021-08-24 15:24:11 +0900

    finished client output MVP

--- public-server.py
+++ public-server.py
@@ -14,13 +14,22 @@ def getPath(path):
     if path == f'{queueRoute}' and request.method == 'POST':
         if request.form['password'] == queuePassword:
             requested = queue[0]
-            queue.pop(0)
             return requested
         else:
             return 'incorrect queue password'
     elif path == f'{queueRoute}' and not request.method=='GET':
         return 'inocrrect method'
-    else:queue.append(path)
+    else:
+        start = time.time()
+        key = len(queue)
+        queue.append({'path':path,'key':key})
+        while time.time()-start <= 5:
+            output = queue[key]['output']
+            if output != '':
+                return output
+            else:
+                continue
+        return '<h1>Sorry...</h1><p>Our servers are busy right now. Please try again in a moment.</p>'
     
 
 if __name__ == '__main__':
