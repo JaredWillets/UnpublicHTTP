e8063a7587d5e044e20175e3f28fcb1bead2d243
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2021-08-25 18:04:49 +0900

    added queue and processing list separation

--- public-server.py
+++ public-server.py
@@ -6,6 +6,7 @@ queueRoute = '[ Insert the path that you would like for the queue ]' ## Replace 
 queue = [] ## This will be used to store the requests that have not been tended to
 queuePassword = 'password' ## Though this code will work, we recommend changing the password here
 returnRoute = '[ Insert the patht ath you would like for information to be returned to ]' ## Replace this or the program will not work
+processing = []
 
 app = Flask(__name__)
 
@@ -15,20 +16,21 @@ def getPath(path):
     if path == f'{queueRoute}' and request.method == 'POST':
         if request.form['password'] == queuePassword:
             requested = queue[0]
+            queue.pop(0)
             return requested
         else:
             return 'incorrect queue password'
-    elif (path == f'{queueRoute}' or path == f'{returnRoute}')and not request.method=='POST':
+    elif (path == f'{queueRoute}' or path == f'{returnRoute}') and not request.method=='POST':
         return 'inocrrect method'
     elif path == f'{returnRoute}' and method == 'POST':
         output = request.form['output']
-        key = request.form['key']
+        key = request.form['processingKey']
+        processing[key]['output'] = output
         return 'ok'
     else:
         start = time.time()
-        key = len(processing)
-        queue.append({'path':path,'key':key,'output':''})
-        processing.append({'path':'key':key,'output':''})
+        queue.append({'path':path,'processingKey':len(processing),'output':''})
+        processing.append({'path':path,'output':''})
         while time.time()-start <= 5:
             output = processing[key]['output']
             if output != '':
