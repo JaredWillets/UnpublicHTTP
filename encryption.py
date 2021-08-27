b0045c0aa3165fe483aa2f3d6717dd970068c860
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2021-08-27 20:36:57 +0900

    made encryption file based

--- encryption.py
+++ encryption.py
@@ -1,15 +1,15 @@
 import rsa
 
+def decrypt(message):
 
-def decrypt(message, PrivKey):
+    PrivKey = rsa.PrivateKey.load_pkcs1(open('PrivKey','rb').read())
 
     return rsa.decrypt(message, PrivKey).decode('utf-8')
 
-def encrypt(message, PubKey):
+def encrypt(message):
+
+    PubKey = rsa.PublicKey.load_pkcs1(open('PubKey','rb').read())
     
     encrypted = rsa.encrypt(message.encode('utf-8'), PubKey)
 
     return encrypted
-
-PubKey, PrivKey = rsa.newkeys(numberOfBits)
-
